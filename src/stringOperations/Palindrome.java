48552c68497d65013bce56c0afa8f9f182eed2ce
janga <mandimalap@gmail.com>
2023-08-30 12:25:24 -0400

    Committing reverse string and Palindrome

--- src/stringOperations/Palindrome.java
+++ src/stringOperations/Palindrome.java
@@ -1,10 +1,23 @@
 package stringOperations;
 
-public class Palindrome 
-{
+import java.util.Scanner;
 
+public class Palindrome extends ReverseString
+{
 	public static void main(String[] args) 
-	{
-		System.out.println("palindrome");
+	{		
+		System.out.println("Input a String to reverse: ");	
+		Scanner objScanner = new Scanner(System.in);
+		String strSource = objScanner.next();
+		
+	//	ReverseString objReverse = new ReverseString();
+	//	String sDeststring = objReverse.ReversedString(strSource);
+		
+		System.out.println("Reversed String:" + ReversedString(strSource) );
+		
+		 if(strSource.equals(ReversedString(strSource)))
+			System.out.println("It is a palindrome"); 
+		 else
+			System.out.println("It is not a palindrome");
 	}
 }
--- src/stringOperations/ReverseString.java
+++ src/stringOperations/ReverseString.java
@@ -4,14 +4,25 @@ import java.util.Scanner;
 
 public class ReverseString 
 {
+	public static String ReversedString(String sourceString)
+	{
+		String sDestString = "";
+		int iStrLength = sourceString.length();
+
+		for(int i=iStrLength-1;i>=0; i--)
+		{
+			sDestString = sDestString+sourceString.charAt(i);
+		}
+		return sDestString;
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Input a String to reverse: ");
-		Scanner objScanner = new Scanner(System.in);
-		
+		Scanner objScanner = new Scanner(System.in);	
 		String strSource = objScanner.next();
 		
 		System.out.println("Entered String: " + strSource);
-		
+		System.out.println("Reversed String: " + ReversedString(strSource));
 	}
 }
